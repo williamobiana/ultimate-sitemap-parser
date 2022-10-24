50ab1d2d3bc64d5975502e4da765f8223d7234f1
williamobiana <williamobiana@gmail.com>
2022-10-24 04:02:21 +0100

    sitemap parser

---
+++ sitemap_parser.py
@@ -0,0 +1,24 @@
+# Find and Parse Sitemaps to Create List of all website's pages
+
+# prerequisites
+# pip install ultimate-sitemap-parser
+from usp.tree import sitemap_tree_for_homepage
+
+def getPagesFromSitemap(fullDomain):
+    #listPagesRaw = []
+    tree = sitemap_tree_for_homepage(fullDomain)
+    with open('log.txt', 'w') as file:
+        for page in tree.all_pages():
+            #listPagesRaw.append(page.url)
+            file.write(page.url +"\n")
+
+
+# Go through List Pages Raw output a list of unique pages links
+def getListUniquePages(filename):
+    listPages = []
+    for page in filename:
+        if page in listPages:
+            pass
+        else:
+            listPages.append(page)
+    print(listPages)
